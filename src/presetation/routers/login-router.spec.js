adab48b4fb44ca0062396dc7956daa88bedb695f
Davi Oliveirab <davioliveira.java@gmail.com>
2020-08-09 19:29:24 -0300

    ensure LoginRouter calls EmailValidator with correct email

--- src/presetation/routers/login-router.spec.js
+++ src/presetation/routers/login-router.spec.js
@@ -40,6 +40,7 @@ const makeAuthUseCaseWithError = () => {
 const makeEmailValidator = () => {
   class EmailValidatorSpy {
     isValid (email) {
+      this.email = email
       return this.isEmailValid
     }
   }
@@ -226,5 +227,17 @@ describe('Login Router', () => {
     }
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    await sut.route(httpRequest)
+    expect(emailValidatorSpy.email).toBe(httpRequest.body.email)
   })
 })
