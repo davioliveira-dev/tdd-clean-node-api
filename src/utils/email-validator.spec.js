68d25995339104b88579f428f21dfa9c50e88102
Davi Oliveirab <davioliveira.java@gmail.com>
2020-08-09 21:08:41 -0300

    ensure EmailValidator returns false if validator returns false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,13 +1,22 @@
+const validator = require('email-validator')
 class EmailValidator {
   isValid (email) {
-    return true
+    const validation = validator.validate(email)
+    return validation
   }
 }
 
 describe('Email Validator', () => {
   test('Should return true if validator returns true', () => {
     const sut = new EmailValidator()
-    const isEmailValid = sut.isValid('valid_email@email.com')
+    const isEmailValid = sut.isValid('davioliveira@gmail.com')
     expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email@email.com')
+    expect(isEmailValid).toBe(false)
   })
 })
